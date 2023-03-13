b2658d3ad5ca318c732599418c9828fcc117b428
Tanguy Verhaegen <tanguy_job@hotmail.fr>
2023-03-13 07:04:49 +0100

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
